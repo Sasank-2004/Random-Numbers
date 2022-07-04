61d6ca2ae42abb5cbefa1cca8b9953f3fb5099f1
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-07-04 18:40:55 +0530

    Update 2.2.py

--- codes/2.2.py
+++ codes/2.2.py
@@ -1,16 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import mpmath as mp
+import math
+
+def F(x):
+
+	return (1+mp.erf(x/math.sqrt(2)))/2
 
 x = np.linspace(-4,4,30)
 simlen = int(1e6)
 err = [] 
+y = []
 randvar = np.loadtxt('gau.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) 
 	err_n = np.size(err_ind) 
-	err.append(err_n/simlen) 
+	err.append(err_n/simlen)
+	y.append(F(x[i])) 
 
 plt.plot(x.T,err)
+plt.plot(x,y,'o')
+plt.legend(["Numerical","Theory"])
 plt.grid() 
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
