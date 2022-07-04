3a398289613c43f79022ef76e522ce0628755707
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-07-04 18:40:27 +0530

    Update 1.2.py

--- codes/1.2.py
+++ codes/1.2.py
@@ -1,16 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def F(x):
+	if (x<0) : return 0
+	elif (x<1) : return x
+	else : return 1
 x = np.linspace(-4,4,30)
 simlen = int(1e6)
 err = [] 
+y = []
 randvar = np.loadtxt('uni.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) 
 	err_n = np.size(err_ind) 
 	err.append(err_n/simlen) 
+	y.append(F(x[i]))
 
 plt.plot(x.T,err)
+plt.plot(x,y,'o')
+plt.legend(["Numerical","Theory"])
 plt.grid() 
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
