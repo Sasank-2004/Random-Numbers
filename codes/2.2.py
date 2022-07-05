640eb2887b5d2a922f7ab076c7f2f93a7548ee91
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-07-05 09:43:16 +0530

    vectorized the cdf function

--- codes/2.2.py
+++ codes/2.2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpmath as mp
 import math
+import scipy
 
 def F(x):
 
@@ -10,16 +11,15 @@ def F(x):
 x = np.linspace(-4,4,30)
 simlen = int(1e6)
 err = [] 
-y = []
 randvar = np.loadtxt('gau.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) 
 	err_n = np.size(err_ind) 
 	err.append(err_n/simlen)
-	y.append(F(x[i])) 
 
+vec_y = scipy.vectorize(F,otypes=[float])
 plt.plot(x.T,err)
-plt.plot(x,y,'o')
+plt.plot(x,vec_y(x),'o')
 plt.legend(["Numerical","Theory"])
 plt.grid() 
 plt.xlabel('$x$')
