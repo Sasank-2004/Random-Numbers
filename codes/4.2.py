777f72405093c3b2d150edefb4092b4212587c3f
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-07-08 11:48:04 +0530

    Update 4.2.py

--- codes/4.2.py
+++ codes/4.2.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
+def F(x):
+	if(x<0) : return 0
+	elif(x<1) : return x*x/2
+	elif(x<2) : return 1 - (2-x)*(2-x)/2
+	else : return 1
+	
 x = np.linspace(-4,4,30)
 simlen = int(1e6)
 err = [] 
@@ -10,7 +17,10 @@ for i in range(0,30):
 	err_n = np.size(err_ind) 
 	err.append(err_n/simlen) 
 
+vec_y = scipy.vectorize(F,otypes=[float])
 plt.plot(x.T,err)
+plt.plot(x,vec_y(x),'o')
+plt.legend(['Numerical','Theory'])
 plt.grid() 
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
