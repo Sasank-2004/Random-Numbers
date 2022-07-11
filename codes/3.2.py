bf5ba0cfa4d6322ea0a116338c9fc78ffc549fcc
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-07-11 23:28:51 +0530

    Update 3.2.py

--- codes/3.2.py
+++ codes/3.2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import scipy
 
 def F(x):
 	if (x<0) : return 0
@@ -8,16 +9,15 @@ def F(x):
 x = np.linspace(-4,4,30)
 simlen = int(1e6)
 err = [] 
-y = []
 randvar = np.loadtxt('var.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) 
 	err_n = np.size(err_ind) 
 	err.append(err_n/simlen) 
-	y.append(F(x[i]))
 	
+vec_y = scipy.vectorize(F,otypes=[float])	
 plt.plot(x.T,err)
-plt.plot(x,y,'o')
+plt.plot(x,vec_y(x),'o')
 plt.legend(["Numerical","Theory"])
 plt.grid() 
 plt.xlabel('$x$')
